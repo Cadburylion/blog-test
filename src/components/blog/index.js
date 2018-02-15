cdeb5175ed3f31ba82616cd3893d2d7ccc43f4be
Matthew Parker <cadburylion@outlook.com>
2018-02-14 16:30:50 -0800

    Enhanced styling for unique image sizes

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -3,13 +3,13 @@ import withFetching from '../fetch'
 import BlogField from './blog-field.js'
 
 const url = 'https://api.rss2json.com/v1/api.json'
-const feed = '?rss_url=https://medium.jasonmdesign.com/feed'
+// const feed = '?rss_url=https://medium.jasonmdesign.com/feed'
+const feed = '?rss_url=https://medium.com/feed/@stanfordmag'
 const key = '&api_key=splntegknn4wmc7f6wc1el2ry4eoj55tdyexwjhv'
 
 const Blog = ({ data, isLoading, error }) => {
   let articles = data.items ? data.items : []
   let title = data.feed ? data.feed.title : ''
-
   if (error) {
     return <p>{error.message}</p>
   }
